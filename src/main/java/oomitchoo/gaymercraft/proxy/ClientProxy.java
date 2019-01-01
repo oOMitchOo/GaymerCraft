7d5e8546bd484b8093672df678667d0844c1b418
oOMitchOo <mitch@knowcraft.net>
2019-01-01 20:38:14 +0100

    Removed now unused client proxy methods

--- src/main/java/oomitchoo/gaymercraft/proxy/ClientProxy.java
+++ src/main/java/oomitchoo/gaymercraft/proxy/ClientProxy.java
@@ -1,7 +1,6 @@
 package oomitchoo.gaymercraft.proxy;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemMeshDefinition;
@@ -9,16 +8,10 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import oomitchoo.gaymercraft.block.BlockHedge;
-import oomitchoo.gaymercraft.block.baseslabs.BlockStoneVertSlab1;
-import oomitchoo.gaymercraft.block.baseslabs.BlockStoneVertSlab2;
-import oomitchoo.gaymercraft.block.baseslabs.BlockWoodVertSlab1;
-import oomitchoo.gaymercraft.block.baseslabs.BlockWoodVertSlab2;
 import oomitchoo.gaymercraft.client.BlockColorsHedge;
 import oomitchoo.gaymercraft.client.ItemColorsHedge;
 import oomitchoo.gaymercraft.client.renderer.entity.RenderUnicorn;
@@ -32,36 +25,6 @@ import javax.annotation.Nonnull;
  * Created by oOMitchOo on 21.11.2018.
  */
 public class ClientProxy extends CommonProxy{
-
-    @Override
-    public void registerItemRendererNoSubt(Item item, int metaData, String regNameAddition, String id) {
-        ModelLoader.setCustomModelResourceLocation(item, metaData, new ModelResourceLocation(item.getRegistryName()+regNameAddition, id));
-    }
-
-    @Override
-    public void registerItemRendererWithSubt(ItemBlock itemBlock, String id) { //todo: UNBEDINGT elegantere Lösung, damit ich nicht für jedes Item(-Block) mit Subtypes ein eigenes if-statement machen muss.
-        if (itemBlock.getBlock() instanceof BlockStoneVertSlab1) {
-            for (BlockStoneVertSlab1.EnumType blockslabs$enumtype : BlockStoneVertSlab1.EnumType.values()) {
-                ModelLoader.setCustomModelResourceLocation(itemBlock, blockslabs$enumtype.getMetadata(), new ModelResourceLocation(itemBlock.getRegistryName()+"_"+blockslabs$enumtype.getName(), id));
-            }
-        } else if (itemBlock.getBlock() instanceof BlockStoneVertSlab2) {
-            for (BlockStoneVertSlab2.EnumType blockslabs$enumtype : BlockStoneVertSlab2.EnumType.values()) {
-                ModelLoader.setCustomModelResourceLocation(itemBlock, blockslabs$enumtype.getMetadata(), new ModelResourceLocation(itemBlock.getRegistryName()+"_"+blockslabs$enumtype.getName(), id));
-            }
-        } else if (itemBlock.getBlock() instanceof BlockWoodVertSlab1) {
-            for (BlockWoodVertSlab1.EnumType blockslabs$enumtype : BlockWoodVertSlab1.EnumType.values()) {
-                ModelLoader.setCustomModelResourceLocation(itemBlock, blockslabs$enumtype.getMetadata(), new ModelResourceLocation(itemBlock.getRegistryName()+"_"+blockslabs$enumtype.getName(), id));
-            }
-        } else if (itemBlock.getBlock() instanceof BlockWoodVertSlab2) {
-            for (BlockWoodVertSlab2.EnumType blockslabs$enumtype : BlockWoodVertSlab2.EnumType.values()) {
-                ModelLoader.setCustomModelResourceLocation(itemBlock, blockslabs$enumtype.getMetadata(), new ModelResourceLocation(itemBlock.getRegistryName()+"_"+blockslabs$enumtype.getName(), id));
-            }
-        } else if (itemBlock.getBlock() instanceof BlockHedge) {
-            for (BlockPlanks.EnumType blockleaves$enumtype : BlockPlanks.EnumType.values()) {
-                ModelLoader.setCustomModelResourceLocation(itemBlock, blockleaves$enumtype.getMetadata(), new ModelResourceLocation(itemBlock.getRegistryName()+"_"+blockleaves$enumtype.getName(), id));
-            }
-        }
-    }
 
     @Override
     public void mapFluidState(Block block, Fluid fluid) {
--- src/main/java/oomitchoo/gaymercraft/proxy/IProxy.java
+++ src/main/java/oomitchoo/gaymercraft/proxy/IProxy.java
@@ -1,16 +1,12 @@
 package oomitchoo.gaymercraft.proxy;
 
 import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fluids.Fluid;
 
 /**
  * Created by oOMitchOo on 21.11.2018.
  */
 public interface IProxy {
-    void registerItemRendererNoSubt(Item item, int metaData, String regNameAddition, String id);
-    void registerItemRendererWithSubt(ItemBlock itemBlock, String id);
     void mapFluidState(Block block, Fluid fluid);
     void registerEntityRenderer();
     void registerColoredBlocks();
