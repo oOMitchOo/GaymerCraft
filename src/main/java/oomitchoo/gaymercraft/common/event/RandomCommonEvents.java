f09fcf0ab0f7bd1f8f59f13dd42a8d6891a2322c
oOMitchOo <mitja.geertz@yahoo.de>
2019-11-12 21:26:01 +0100

    Added some particles and stuff, when turning horse into unicorn
    Added some particles (hearts, poof) on the client side, when turning
    a horse into a unicorn.
    
    Also the unicorn will rear and make a noise (server side).

--- src/main/java/oomitchoo/gaymercraft/common/event/RandomCommonEvents.java
+++ src/main/java/oomitchoo/gaymercraft/common/event/RandomCommonEvents.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.horse.HorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -13,6 +14,8 @@ import oomitchoo.gaymercraft.entity.UnicornEntity;
 import oomitchoo.gaymercraft.init.ModEntities;
 import oomitchoo.gaymercraft.init.ModItems;
 import oomitchoo.gaymercraft.reference.Reference;
+
+import java.util.Random;
 
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID)
 public class RandomCommonEvents {
@@ -30,22 +33,37 @@ public class RandomCommonEvents {
     }
 
     private static void exchangeHorseWithUnicornIfRainbowItem(ItemStack itemStack, World world, PlayerEntity player, Entity target) {
-        if(!world.isRemote() && itemStack.getItem().equals(ModItems.RAINBOW_STAR_CHARGED)) {
-            if(target instanceof HorseEntity && !((HorseEntity) target).isTame()) {
-                UnicornEntity replaceWith = new UnicornEntity(ModEntities.UNICORN, world);
-                replaceWith.setLocationAndAngles(target.posX, target.posY, target.posZ, target.rotationYaw, target.rotationPitch);
-                replaceWith.setRotationYawHead(target.getRotationYawHead());
-                replaceWith.setGrowingAge(((HorseEntity) target).getGrowingAge());
 
-                replaceWith.setRandomUnicornVariant();
-                replaceWith.setHorseTamed(true);
-                replaceWith.setTamedBy(player);
+        if(itemStack.getItem().equals(ModItems.RAINBOW_STAR_CHARGED)) {
+            if (target instanceof HorseEntity && !((HorseEntity) target).isTame()) {
+                if (!world.isRemote()) {
+                    UnicornEntity replaceWith = new UnicornEntity(ModEntities.UNICORN, world);
+                    replaceWith.setLocationAndAngles(target.posX, target.posY, target.posZ, target.rotationYaw, target.rotationPitch);
+                    replaceWith.setRotationYawHead(target.getRotationYawHead());
+                    replaceWith.setGrowingAge(((HorseEntity) target).getGrowingAge());
 
-                target.remove();
-                world.addEntity(replaceWith);
+                    replaceWith.setRandomUnicornVariant();
+                    replaceWith.setHorseTamed(true);
+                    replaceWith.setTamedBy(player);
 
-                if(!player.isCreative())
-                    player.setHeldItem(Hand.MAIN_HAND, ModItems.RAINBOW_STAR_UNCHARGED.getDefaultInstance());
+                    target.remove();
+                    world.addEntity(replaceWith);
+                    replaceWith.makeMad();
+
+                    if(!player.isCreative())
+                        player.setHeldItem(Hand.MAIN_HAND, ModItems.RAINBOW_STAR_UNCHARGED.getDefaultInstance());
+                } else {
+                    Random rand = new Random();
+                    for (int i = 0; i < 30; i++) {
+                        double d0 = rand.nextGaussian() * 0.02D;
+                        double d1 = rand.nextGaussian() * 0.02D;
+                        double d2 = rand.nextGaussian() * 0.02D;
+                        double d3 = rand.nextDouble() * (double)target.getWidth() * 2.0D - (double)target.getWidth();
+                        double d4 = 0.5D + rand.nextDouble() * (double)target.getHeight();
+                        double d5 = rand.nextDouble() * (double)target.getWidth() * 2.0D - (double)target.getWidth();
+                        world.addParticle((i < 15) ? ParticleTypes.POOF : ParticleTypes.HEART, target.posX + d3, target.posY + d4, target.posZ + d5, d0, d1, d2);
+                    }
+                }
             }
         }
     }
